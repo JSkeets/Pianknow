1ae74eee79e93cd3d0595bbd3ad09f10d69ba0e5
Jesse Skeets <jesseskeets@gmail.com>
2018-03-05 22:10:38 -0800

    Navbar component

---
+++ pianknow/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> HELLO </h1>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
